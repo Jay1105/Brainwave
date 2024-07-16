998fd9f7f66eef0e535ce4f41f8bcec3445dcd5e
Jay Chotalia <75968475+Jay1105@users.noreply.github.com>
2024-07-16 12:46:24 +0530

    Add files via upload

---
+++ src/assets/svg/Brackets.jsx
@@ -0,0 +1,27 @@
+const brackets = (position) =>
+  position === "left" ? (
+    <svg
+      width="5"
+      height="14"
+      viewBox="0 0 5 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M5 0.822266H1V12.8223H5" stroke="#ada8c3" />
+    </svg>
+  ) : (
+    <svg
+      width="5"
+      height="14"
+      viewBox="0 0 5 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M-2.98023e-08 0.822266H4V12.8223H-2.98023e-08"
+        stroke="#ada8c3"
+      />
+    </svg>
+  );
+
+export default brackets;
